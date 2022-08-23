a28bf15b21d2f4aa1d56450b3dd3b777aa0841fe
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-23 15:49:16 +0000

    t33333

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-i = 97
-while i < 123:
+i = 96
+while i < 122:
+    i += 1
     if i == 101 or i == 113:
-        print("{}".format(''), end='')
+        continue
     else:
         print("{}".format(chr(i)), end='')
-    i += 1
