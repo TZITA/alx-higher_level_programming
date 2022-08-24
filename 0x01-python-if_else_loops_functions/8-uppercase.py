c418dc307d2f0e12dc5c1ccb3ba9b7a602e554e8
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-24 16:35:56 +0000

    rev 2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     l_len = len(str)
-    while i < l_len):
+    i = 0
+    while i < l_len:
         if 97 <= ord(str[i]) <= 122:
             m = chr(ord(str[i]) + 32)
             print("{}".format(m), end='')
