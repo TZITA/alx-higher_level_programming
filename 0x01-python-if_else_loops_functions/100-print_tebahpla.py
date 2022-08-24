780addfcd70845aee85f740fb609e1524c8f0bd0
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-24 00:24:56 +0000

    t141414

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 i = 122
 while i > 96:
-    if i % 2 != 0:
-        i = i - 32
+    if i % 2 == 0:
         print("{}".format(chr(i)), end='')
-        continue
     else:
+        num = i - 32
         print("{}".format(chr(i)), end='')
     i -= 1
