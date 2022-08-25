7d48f0aa3b9cf9f76fec07a81046abb3993649b1
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-25 20:39:48 +0000

    t100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+if __name__ == "__main__":
+    n = len(sys.argv)
+    if n != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    elif sys.argv[2] != '+':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    elif sys.argv[2] != '-':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    elif sys.argv[2] != '*':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    elif sys.argv[2] != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    else:
+        if sys.argv[2] == '+':
+            add1 = add(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} + {} = {}".format(sys.argv[1], sys.argv[3], add1))
+        if sys.argv[2] == '-':
+            sub1 = sub(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} - {} = {}".format(sys.argv[1], sys.argv[3], sub1))
+        if sys.argv[2] == '*':
+            mul1 = mul(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} * {} = {}".format(sys.argv[1], sys.argv[3], mul1))
+        if sys.argv[2] == '/':
+            div1 = div(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} / {} = {}".format(sys.argv[1], sys.argv[3], div1))
+        
