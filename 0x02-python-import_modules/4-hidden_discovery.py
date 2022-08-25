677f47b476e89d5843effac487ce00173d03bd56
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-25 22:11:48 +0000

    t444

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-import hidden_4 
-print(dir())
+import hidden_4
+a = dir(hidden_4)
+n = len(a) - 1
+for i in range(0, n):
+    e = a.[i]
+    if e.startswith("__"):
+        continue
+    else:
+        print("{}".format(e))
