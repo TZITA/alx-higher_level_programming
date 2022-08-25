3dc3fa5b58ddc948edf1f3cc9c7f7f6c4ceaef0a
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-25 14:52:54 +0000

    t1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ = "__main__":
+    a = 10
+    b = 5
+    add1 = add(a, b)
+    sub1 = sub(a, b)
+    mul1 = mul(a, b)
+    div1 = div(a, b)
+    print("{} + {} = {}".format(a, b, add1))
+    print("{} - {} = {}".format(a, b, sub1))
+    print("{} * {} = {}".format(a, b, mul1))
+    print("{} / {} = {}".format(a, b, div1))
