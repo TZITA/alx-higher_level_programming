762cdd6b73bea3083d45324df603e1752836f00e
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-25 16:19:44 +0000

    t222

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 import sys
-if __name == "__main_":
-    i = 0
-    if len(sys.argv) == 2:
-        print("{} argument:".format (len(sys.argv)))
-        print("1: {}".format(sys.argv[1])) 
-    elif sys.argv == 1:
-        print("{} arguments.".format (len(sys.argv)))
-    else:        
-        print("{} arguments:".format (len(sys.argv)))
-        while i < len(sys.argv):
-            print("{}: {}".format((i + 1) ,sys.argv[i]))
-            i += 1
+n = len(sys.argv)
+if n == 2:
+    print("{} argument:".format (n - 1))
+    print("1: {}".format(str(sys.argv[1])))
+elif n == 1:
+    print("{} arguments.".format (n - 1))
+else:        
+    print("{} arguments:".format (n - 1))
+    for i in range(1, n):
+        print("{}: {}".format(i,str(sys.argv[i])))
