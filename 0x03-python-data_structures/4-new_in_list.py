3a23148db1512a9f002658ac0a726098e9e594ca
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-27 11:00:43 +0000

    t44444

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    range_n = len(my_list) - 1
-    
+    range_n = len(my_list) - 1    
     if idx < 0 or idx > range_n:
         return my_list.copy()
     else:
