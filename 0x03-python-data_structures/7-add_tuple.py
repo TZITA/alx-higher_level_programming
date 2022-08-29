3da3126c7a29a5a846f885adf7094e74cf98f55a
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-29 19:19:48 +0000

    t77

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,6 +5,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) == 0:
         tuple_b = (0, 0)
     elif len(tuple_b) == 1:
-        tuple_b = (1, 0)
+        tuple_b = (tuple_b[0], 0)
     new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return new_tuple
