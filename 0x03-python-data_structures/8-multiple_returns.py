4f2098406ecbc3287240dae7f9459b7413ac18ea
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-29 19:16:46 +0000

    t8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     len_s = len(sentence)
-    first_letter = sentence[0]
     if len_s == 0:
-        first_letter = None
-    return (len_s, first_letter)
+        return (len_s, None)
+    else:
+        first_letter = sentence[0]
+        return (len_s, first_letter)
