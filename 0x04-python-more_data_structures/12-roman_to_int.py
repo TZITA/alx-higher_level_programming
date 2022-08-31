cb7cff824756cc3b23203b24c4717847bc185156
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-31 19:56:30 +0000

    t12 final

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -14,18 +14,30 @@ def roman_to_int(roman_string):
                 else:
                     tobeadded.append(5)
             elif roman_string[i] == "X":
-                if roman_string[i - 1] == "I":
+                if i > 0 and roman_string[i - 1] == "I":
                     tobeadded.append(8)
                 else:
                     tobeadded.append(10)
             elif roman_string[i] == "L":
-                tobeadded.append(50)
+                if i > 0 and roman_string[i - 1] == "X":
+                    tobeadded.append(30)
+                else:
+                    tobeadded.append(50)
             elif roman_string[i] == "C":
-                tobeadded.append(100)
+                if i > 0 and roman_string[i - 1] == "X":
+                    tobeadded.append(80)
+                else:
+                    tobeadded.append(100)
             elif roman_string[i] == "D":
-                tobeadded.append(500)
+                if i > 0 and roman_string[i - 1] == "C":
+                    tobeadded.append(300)
+                else:
+                    tobeadded.append(500)
             elif roman_string[i] == "M":
-                tobeadded.append(1000)
+                if i > 0 and roman_string[i - 1] == "C":
+                    tobeadded.append(800)
+                else:
+                    tobeadded.append(1000)
         for j in range(len(tobeadded)):
             sum1 += int(tobeadded[j])
         return sum1
