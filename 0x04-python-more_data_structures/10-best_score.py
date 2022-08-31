689befd642dec7575b1ee6cf0a1decf81cc66d23
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-31 22:15:48 +0000

    t10fff

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,8 +8,8 @@ def best_score(a_dictionary):
         for k in lis:
             if a_dictionary[k] > max_val:
                 max_val = a_dictionary[k]
-        if max_val > 0:
+        if max_val is None:
+            return None
+        else:
             key = {k for k, v in a_dictionary.items() if v == max_val}
             return list(key)[0]
-        else:
-            return None
