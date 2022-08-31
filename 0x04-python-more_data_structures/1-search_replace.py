005875a252e1f87b74a896780fad4dfd2e273389
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-31 14:45:57 +0000

    t11

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list1 = my_list.copy() 
+    my_list1 = my_list.copy()
     for i in range(len(my_list1)):
         if my_list1[i] == search:
             my_list1[i] = replace
