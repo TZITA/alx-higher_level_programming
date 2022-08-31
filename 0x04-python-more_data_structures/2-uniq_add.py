d888ef1c68eaab8a5013946d8d51785d7270ed70
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-31 12:06:21 +0000

    t22

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    item = my_list[0]
-    taddl = [my_list[0]]
+    aset = set(my_list)
+    alist = list(aset)    
     sum1 = 0
-    for i in range(0, len(my_list)):
-        if my_list[i] != item:
-            taddl.append(my_list[i])
-    for j in range(0, len(taddl)):
-        sum1 += int(my_list[j])
+    for i in range(0, len(alist)):
+        sum1 += int(alist[i])
     return sum1
