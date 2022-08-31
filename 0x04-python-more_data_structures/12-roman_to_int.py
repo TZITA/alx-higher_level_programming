6e5ed0d0ff49807bf0e104239bb541aeaf1c87d2
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-31 19:36:33 +0000

    t1212

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    tobeadded = []
-    sum1 = 0
-    for i in range(len(roman_string)):
-        if roman_string[i] =="I":
-             tobeadded.append(1)
-        elif roman_string[i] == "V":
-             tobeadded.append(5)
-        elif roman_string[i] == "X":
-             tobeadded.append(10)
-        elif roman_string[i] == "L":
-             tobeadded.append(50)
-        elif roman_string[i] == "C":
-             tobeadded.append(100)
-        elif roman_string[i] == "D":
-             tobeadded.append(500)
-        elif roman_string[i] == "M":
-             tobeadded.append(1000)
-    for j in range(len(tobeadded)):
-        sum1 += int(tobeadded[j])
-    return sum1
+    if roman_string is None:
+        return 0
+    else:
+        tobeadded = []
+        sum1 = 0
+        for i in range(len(roman_string)):
+            if roman_string[i] =="I":
+                tobeadded.append(1)
+            elif roman_string[i] == "V":
+                tobeadded.append(5)
+            elif roman_string[i] == "X":
+                tobeadded.append(10)
+            elif roman_string[i] == "L":
+                tobeadded.append(50)
+            elif roman_string[i] == "C":
+                tobeadded.append(100)
+            elif roman_string[i] == "D":
+                tobeadded.append(500)
+            elif roman_string[i] == "M":
+                tobeadded.append(1000)
+        for j in range(len(tobeadded)):
+            sum1 += int(tobeadded[j])
+        return sum1
