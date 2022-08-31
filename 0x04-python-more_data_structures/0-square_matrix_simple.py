d7bd16b11cf4384691b0ed225518360de01f56e9
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-31 23:08:26 +0000

    t0000

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     cp_matrix = matrix.copy()
-    listoflist = []
-    for i in range(len(cp_matrix)):
-        listoflist.append(cp_matrix[i])
-    for j in range(len(listoflist)):
-        for k in range(len(listoflist[j])):
-            listoflist[j][k] *= listoflist[j][k]
-    return listoflist
+    for j in range(len(cp_matrix)):
+        for k in range(len(cp_matrix[j])):
+            cp_matrix[j][k] *= cp_matrix[j][k]
+    return cp_matrix
