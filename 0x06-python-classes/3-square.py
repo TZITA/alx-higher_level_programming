c17f544567cfe47b54e717ebbd95cb8b3a250cfd
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-13 18:41:37 +0000

    t3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square representation."""
+
+
+class Square:
+    """A class that define a square."""
+
+    def __init__(self, size=0):
+        """Initializing a square.
+           Attributes:
+           size: size of the square.
+        """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
+
+
+    def area(self):
+        """Returns current square area."""
+        return (self.__size * self.__size)
