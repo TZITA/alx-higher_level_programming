4cb9529caf4ec834a30803402bdacec7c8918e26
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-13 14:55:49 +0000

    t1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class that defines Sqaure."""
+
+
+class Square:
+    """Represents a square.
+
+    Attributes:
+        size: Size of the square
+    """
+    
+
+    def __init__(self, size):
+        self.__size = size
+
