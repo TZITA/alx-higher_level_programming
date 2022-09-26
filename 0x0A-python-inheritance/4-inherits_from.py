c33841ca84ea38beb9eb55de35b16b456afeb0b7
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-26 14:10:24 +0000

    t4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,5 +14,5 @@ def inherits_from(obj, a_class):
         False = otherwise.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
-         return (True)
+        return (True)
     return (False)
