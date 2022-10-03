73c56ad0e8b45985e4204eeac61280db0b775670
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-10-03 13:29:54 +0000

    t16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a class Base."""
+import json
 
 
 class Base:
@@ -13,3 +14,27 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list of dictionaries.
+        Args:
+            1. list_dictionary: List of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file.
+        Args:
+            1. list_objs: List of inherited base instances.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dictionary = [i.to_dictionary() for i in list_objs]
+                file.write(Base.to_json_string(list_dictionary))
