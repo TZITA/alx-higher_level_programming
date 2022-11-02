0f8b0f4491f764ab680a5ed2d3714a818b26c5db
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-11-02 21:20:12 +0300

    all

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+-- Lists Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT `name`, SUM(`rate`) AS `rating`
+  FROM `tv_genres` AS gr
+       INNER JOIN `tv_show_genres` AS st
+       ON st.`genre_id` = gr.`id`
+
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = st.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
