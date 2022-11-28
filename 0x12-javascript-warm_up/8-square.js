f556bc816edcde0d10f362e64f925594bfde176a
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-11-28 23:30:17 +0300

    t8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const times = process.argv[2];
+if (isNaN(times)) {
+	console.log('Missing size');
+} else {
+	for (let i = 0; i < times; i++) {
+		let row = '';
+		for (let j = 0; j < times; j++) {
+			row += 'X';
+		}
+		console.log(row);
+	}
+}
