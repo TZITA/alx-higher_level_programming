5124d4a6315a82aa2944f0bdf1352c8d8621724a
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-11-28 23:43:01 +0300

    t9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-	let sum = a + b;
-	return sum;
+function add (a, b) {
+  const sum = a + b;
+  return sum;
 }
 console.log(add(Number(process.argv[2]), Number(process.argv[3])));
