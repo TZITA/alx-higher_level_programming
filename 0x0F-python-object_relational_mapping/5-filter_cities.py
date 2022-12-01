d9aab88af14a663377c39627b273af9cb47d0d08
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-12-01 20:24:43 +0300

    t5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = conn.cursor()
+    c.execute("SELECT c.id, c.name, s.name \
+            FROM cities AS c \
+            INNER JOIN states AS s \
+            ON c.state_id = s.id \
+            ORDER BY c.id")
+    cc = []
+    for city in c.fetchall():
+        if city[2] == sys.argv[4]:
+            cc.append(city[1])
+    print(", ".join(cc))
