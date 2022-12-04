cb7544c42659b1e4ae6756bbf90fccfeebc8dbf5
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-12-04 19:13:57 +0300

    t3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Displays values in states where name matches argument. Safe from MySQL injections!"""
+"""Displays values in states where name=arg(Safe from MySQL injections)"""
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = conn.cursor()
+    co = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = co.cursor()
     c.execute("SELECT * FROM states")
     [print(st) for st in c.fetchall() if st[1] == sys.argv[4]]
