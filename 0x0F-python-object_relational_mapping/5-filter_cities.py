20e411abebd8cbe825f79d90600e698f0cd38425
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-12-04 19:15:39 +0300

    t3

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,8 +5,8 @@ import MySQLdb
 
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = conn.cursor()
+    co = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = co.cursor()
     c.execute("SELECT c.id, c.name, s.name \
             FROM cities AS c \
             INNER JOIN states AS s \
