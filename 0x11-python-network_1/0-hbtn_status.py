ab5da779448bd04ac14f2da0937ea40fe038c298
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-12-16 15:13:53 +0300

    t0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,5 +5,9 @@ import urllib.request
 
 if __name__ == '__main__':
     r = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen(r) as res:
         result = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(result)))
+        print("\t- content: {}".format(result))
+        print("\t- utf8 content: {}".format(result.decode("utf-8")))
