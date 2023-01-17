c2e3477563d924d68d621e4b9478be78626d9730
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-01-17 19:16:53 +0300

    t3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+let t_url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(t_url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
