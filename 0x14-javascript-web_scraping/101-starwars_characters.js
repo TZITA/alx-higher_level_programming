56d3a9310ecbeef51ca012bce47d308e65575f68
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-01-17 18:31:40 +0300

    adv

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    let characters = JSON.parse(body).characters;
+    printChar(characters, 0);
+  }
+});
+
+function printChar (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printChar(characters, index + 1);
+      }
+    }
+  });
+}
