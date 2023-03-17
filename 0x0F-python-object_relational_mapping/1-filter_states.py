4f1945c5b6f3fb247c1ae4ea054ff1f0cb330460
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-03-17 11:34:50 +0300

    t1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = con.cursor()
     c.execute("SELECT * FROM states")
-    [print (state) for state in c.fetchall() if state[1][0] == 'N']
+    [print(state) for state in c.fetchall() if state[1][0] == 'N']
