a20d9c190f6a2c8fcbee77e1a86ccd3a5a720596
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-03-30 20:01:27 +0300

    Initial Commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response.
-
-curl -Iso ~/111 $1
-grep -o '(?<=Content-Length: )\d+' ~/111
-rm ~/111
+# Takes in a URL, sends a request, and displays the size
+curl -Is $1 | grep -o '(?<=Content-Length: )\d+'
