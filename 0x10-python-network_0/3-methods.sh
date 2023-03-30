816f269350f3e0fbc2bf07d6e573edf00803b1ba
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-03-30 22:41:03 +0300

    used wc command and made the file executable

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.
-curl -sIX OPTIONS $1 | sed -n 's/^Allow: \(.*\).*/\1/p'
+curl -sIX OPTIONS "$1" | sed -n 's/^Allow: \(.*\).*/\1/p'
