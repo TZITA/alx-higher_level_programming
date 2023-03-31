da7c68659d32a2bd6d2d7aa0ef11b8abfa43f314
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-03-31 22:23:09 +0300

    search api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,9 +10,9 @@ import requests
 if __name__ == "__main__":
     if len(sys.argv) > 0:
         val = sys.argv[1]
-        res = requests.post('http://0.0.0.0:5000/search_user', data={"q":val})
+        res = requests.post('http://0.0.0.0:5000/search_user', data={"q": val})
     else:
-        res = requests.post('http://0.0.0.0:5000/search_user', data={"q":""})
+        res = requests.post('http://0.0.0.0:5000/search_user', data={"q": ""})
     try:
         json_d = res.json()
         if json_d == {}:
